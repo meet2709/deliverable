de10178edf0c139e0c44ef0edfb7900244551b66
user <user@LAPTOP-MO3LOTSK>
2020-04-19 19:34:38 -0400

    del3

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -5,7 +5,7 @@ package ca.sheridancollege.project;
  * enough to be instantiated for any Card game. Students wishing to add to the code
  * should remember to add themselves as a modifier.
  * @author dancye, 2018
- */
+ *///meet thakkar
 public abstract class Card {
 
 	/**
